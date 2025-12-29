cae8516ad701e4a37575720ac0b6bcc2ffec533c
Sunil-731820 <sunilg@infinite.com>
2025-12-29 13:11:46 +0530

    User Authentication Code is Completed

--- UserAuthentocation/src/main/java/com/example/UserAuthentocation/Exceptions/OtpInvalidException.java
+++ UserAuthentocation/src/main/java/com/example/UserAuthentocation/Exceptions/OtpInvalidException.java
@@ -3,5 +3,5 @@ package com.example.UserAuthentocation.Exceptions;
 public class OtpInvalidException extends RuntimeException {
     public OtpInvalidException(String message) {
         super(message);
-    }
+    } 
 }
